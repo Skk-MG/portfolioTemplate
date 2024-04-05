e8651eee16c77a5d3233beb4da865ef5df2b82ad
Skk-Martin <martinezequielgarcia495@gmail.com>
2024-04-05 14:03:31 -0300

    timeline & index carousel added

---
+++ src/scripts/script.js
@@ -0,0 +1,19 @@
+document.addEventListener("DOMContentLoaded", function() {
+    let currentSlide = 0;
+    const slides = document.querySelectorAll('.slide');
+    const totalSlides = slides.length;
+  
+    function nextSlide() {
+      currentSlide = (currentSlide + 1) % totalSlides;
+      updateCarousel();
+    }
+  
+    function updateCarousel() {
+      const slideWidth = slides[0].offsetWidth;
+      const newTransformValue = -1 * currentSlide * slideWidth;
+      document.querySelector('.carousel-container').style.transform = `translateX(${newTransformValue}px)`;
+    }
+  
+    setInterval(nextSlide, 2000);
+  });
+  
