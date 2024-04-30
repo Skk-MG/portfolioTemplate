48d07929314f2bcf58894ae8379355b3ab7ad7c7
Skk-Martin <martinezequielgarcia495@gmail.com>
2024-04-30 19:49:24 -0300

    transitions added, got to fix text-slides

--- src/scripts/lightModeSwitch.js
+++ src/scripts/lightModeSwitch.js
@@ -1,27 +1,29 @@
+document.addEventListener('astro:page-load', () => {
 
-const themeToggle = document.querySelector(
-  '.theme-switch input[type="checkbox"]'
-);
+  const themeToggle = document.querySelector(
+    '.theme-switch input[type="checkbox"]'
+  );
 
-const currentTheme = localStorage.getItem("theme");
+  const currentTheme = localStorage.getItem("theme");
 
-if (currentTheme) {
-  document.documentElement.setAttribute("data-theme", currentTheme);
+  if (currentTheme) {
+    document.documentElement.setAttribute("data-theme", currentTheme);
 
-  if (currentTheme === "dark") {
-    themeToggle.checked = true;
+    if (currentTheme === "dark") {
+      themeToggle.checked = true;
+    } 
   }
-}
 
-function switchTheme(e) {
-  if (e.target.checked) {
-    document.documentElement.setAttribute("data-theme", "dark");
-    localStorage.setItem("theme", "dark");
-  } else {
-    document.documentElement.setAttribute("data-theme", "light");
-    localStorage.setItem("theme", "light");
+  function switchTheme(e) {
+    if (e.target.checked) {
+      document.documentElement.setAttribute("data-theme", "dark");
+      localStorage.setItem("theme", "dark");
+    } else {
+      document.documentElement.setAttribute("data-theme", "light");
+      localStorage.setItem("theme", "light");
+    }
   }
-}
 
-themeToggle.addEventListener("change", switchTheme, false);
+  themeToggle.addEventListener("change", switchTheme, false);
 
+});
--- src/scripts/menu.js
+++ src/scripts/menu.js
@@ -1,6 +1,8 @@
-let menuLinks = document.querySelectorAll('.menuLink');
-menuLinks.forEach(function(menuLink) {
-    if (menuLink.getAttribute('href') === window.location.pathname) {
-        menuLink.classList.add('active');
-    }
+document.addEventListener('astro:page-load', () => {
+    let menuLinks = document.querySelectorAll('.menuLink');
+    menuLinks.forEach(function(menuLink) {
+        if (menuLink.getAttribute('href') === window.location.pathname) {
+            menuLink.classList.add('active');
+        }
+    });
 });
